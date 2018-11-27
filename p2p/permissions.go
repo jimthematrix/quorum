376c9bc38ec18973c8bda90d491d65213e3371a8
Vinod Damle <vinod.damle@consensys.net>
2018-11-27 11:39:30 -0800

    Remove debug logs from permissioned nodes check
    geth's autodiscovery discovers huge number of nodes that do not belong
    to a chain and each discovery triggers at least 3 logs with information
    on all permissioned nodes. This chews up a lot of disk over time in
    geth.log. Since we configure debug level logging in quorum/geth, we need
    to remove these logs

--- p2p/permissions.go
+++ p2p/permissions.go
@@ -24,14 +24,14 @@ func isNodePermissioned(nodename string, currentNode string, datadir string, dir
 		permissionedList = append(permissionedList, v.ID.String())
 	}
 
-	log.Debug("isNodePermissioned", "permissionedList", permissionedList)
+	// log.Debug("isNodePermissioned", "permissionedList", permissionedList)
 	for _, v := range permissionedList {
 		if v == nodename {
 			log.Debug("isNodePermissioned", "connection", direction, "nodename", nodename, "ALLOWED-BY", currentNode)
 			return true
 		}
 	}
-	log.Debug("isNodePermissioned", "connection", direction, "nodename", nodename, "DENIED-BY", currentNode)
+	// log.Debug("isNodePermissioned", "connection", direction, "nodename", nodename, "DENIED-BY", currentNode)
 	return false
 }
 
@@ -73,6 +73,5 @@ func parsePermissionedNodes(DataDir string) []*discover.Node {
 		}
 		nodes = append(nodes, node)
 	}
-	log.Debug("parsePermissionedNodes", "permissionedNodeList", nodes)
 	return nodes
 }
